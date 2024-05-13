657bf4509a579562a4e14ab556dc3d1a0acc82ec
Fathony L <fathony@smart-leaders.net>
2024-05-13 07:20:25 +0700

    webamp

--- main/windows.js
+++ main/windows.js
@@ -1,19 +1,80 @@
 var electron = require('electron')
+const { desktopCapturer } = require('electron')
 var BrowserWindow = electron.BrowserWindow
 var ipcMain = electron.ipcMain
+const Screenshot = require('electron-screenshot-app');
 var config = require('./config')
 
 var list = []
 
 ipcMain.on('create-window', create)
 
+
+var win2 = null;
+var lastWin = null
+
+// Ketika Anda ingin menangkap screenshot dari window
+ipcMain.on('capture', async (event, args) => {
+  console.log('receive: capture', args)
+  // const screenshot = await Screenshot.capture(lastWin);
+  // Sekarang, Anda bisa mengirim screenshot ke window lain atau melakukan apa pun yang Anda inginkan dengan gambar tersebut
+  // Misalnya, kirim gambar ke window lain melalui IPC
+  desktopCapturer.getSources({ types: ['screen'] })
+      .then( sources => {
+          // document.getElementById('screenshot-image').src = sources[0].thumbnail.toDataURL() // The image to display the screenshot
+          // Lakukan apa pun yang Anda inginkan dengan screenshot
+          // Misalnya, tampilkan dalam elemen <img> di window ini
+          // const imageElement = document.createElement('img');
+          // imageElement.src = sources[0].thumbnail.toDataURL() // The image to display the screenshot;
+
+          // const screenshot = sources[0].thumbnail.toDataURL() //? its work! The image to display the screenshot;
+          const screenshot = sources[0].thumbnail.toDataURL() // The image to display the screenshot;
+  console.log('captureed', screenshot)
+          // document.body.appendChild(imageElement);
+          // event.sender.send('captured', screenshot);
+          // win2.webContents.send('captured', screenshot);
+          // win2.send('captured', screenshot);
+          event.returnValue = screenshot;
+      })
+});
+
 function create () {
+  if(!win2){
+    win2 = new BrowserWindow({
+      title: config.APP_NAME,
+      y:50,
+      x:500,
+      width: 500,
+      height: 200,
+      acceptFirstMouse: true,
+      opacity: 0.6,
+  
+      // frameless: true,
+      frame: false,
+      transparent: true, 
+      skipTaskbar: true,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+    })
+    win2.loadURL(config.INDEX)
+  }
   var win = new BrowserWindow({
     title: config.APP_NAME,
+    y:50,
+    x:50,
     width: 500,
     height: 200,
-    acceptFirstMouse: true
+    acceptFirstMouse: true,
+
+    // frameless: true,
+    frame: false,
+    transparent: true, 
   })
+  const i = list.length +1
+  lastWin=win;
+  // win.setBounds({x:10 * i, y:20*i})
 
   win.loadURL(config.INDEX)
   win.setTitle(`${config.APP_NAME} - Window ${win.id}`)
@@ -27,6 +88,21 @@ function create () {
 
   win.on('closed', function () {
     destroy(win)
+  })
+
+  win.on('move', function () {
+    // console.log('win.move')
+    // if(win.getBounds().x > 1000) {
+    //   win.setBounds({x:900})
+    // }
+    var b = win.getBounds()
+    win2.setBounds({x: b.x + b.width})
+  })
+  win.on('moved', function () {
+    console.log('win.moved')
+  })
+  win.on('will-move', function () {
+    console.log('win.wil-move')
   })
 }
 
--- renderer/index.js
+++ renderer/index.js
@@ -1,10 +1,49 @@
+document.getElementById('yellow').innerHTML = 'going to require()'
 const ipcRenderer = require('electron').ipcRenderer
+document.getElementById('yellow').innerHTML = 'its now yellow.id'
+document.getElementById('yellow').style.background = 'lime'
+const { desktopCapturer } = require('electron')
+document.getElementById('yellow').style.background = 'aqua'
+
+// document.body.style.opacity = '0.5'
+
+console.log('index was here!')
 
 function createWindow (type) { ipcRenderer.send('create-window', type) }
 
-document.querySelector('#create-window').onclick = createWindow
+document.querySelector('#create-window').addEventListener('click', () => createWindow())
+
+// document.querySelector('#capture').addEventListener('click', (e) => ipcRenderer.send('capture', 'foo'))
+document.querySelector('#capture').addEventListener('click', (e) => {
+  const capture = ipcRenderer.sendSync('capture', 'foo');
+  const imageElement = document.createElement('img');
+          imageElement.src = capture;
+          document.body.appendChild(imageElement);
+})
+document.getElementById('capture0').addEventListener('click', () => { // The button which takes the screenshot
+  desktopCapturer.getSources({ types: ['screen'] })
+      .then( sources => {
+          // document.getElementById('screenshot-image').src = sources[0].thumbnail.toDataURL() // The image to display the screenshot
+          // Lakukan apa pun yang Anda inginkan dengan screenshot
+          // Misalnya, tampilkan dalam elemen <img> di window ini
+          const imageElement = document.createElement('img');
+          imageElement.src = sources[0].thumbnail.toDataURL() // The image to display the screenshot;
+          document.body.appendChild(imageElement);
+      })
+})
 
 ipcRenderer.on('id', function (event, ...args) {
   var id = args[0]
   document.querySelector('#win-id').innerHTML = id
 })
+
+// Ketika menerima screenshot dari window utama
+ipcRenderer.on('captured', (event, screenshot) => {
+  document.getElementById('yellow').style.background = 'fuchsia'
+  // Lakukan apa pun yang Anda inginkan dengan screenshot
+  // Misalnya, tampilkan dalam elemen <img> di window ini
+  document.getElementById('yellow').innerHTML = screenshot;
+  const imageElement = document.createElement('img');
+  imageElement.src = screenshot;
+  document.body.appendChild(imageElement);
+});
